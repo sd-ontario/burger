69b9bc16c6aa41e90ac404cbbde73329e3f040ed
Stephen <stephen.dawe94@gmail.com>
2018-09-04 20:19:46 -0400

    mysql command not working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheddar Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Blue Cheese Onion Mushroom Burger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Avocado Brie Burger", FALSE);
