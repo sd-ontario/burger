debb6d81d7cad917dbe2c3420f55efe7afef331a
Stephen <stephen.dawe94@gmail.com>
2018-09-05 19:20:55 -0400

    DB still not posting to view

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+USE burgers_db;
+
 INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheddar Burger", FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Blue Cheese Onion Mushroom Burger", TRUE);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Avocado Brie Burger", FALSE);
